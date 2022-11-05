4446004e9cbd51f72c0a1292fb67fb079f492cc2
bhr <93179048+544419203@users.noreply.github.com>
2022-11-05 15:14:15 +0800

    Update AllConfig.java

--- src/main/java/com/pt/vx/config/AllConfig.java
+++ src/main/java/com/pt/vx/config/AllConfig.java
@@ -70,7 +70,7 @@ public class AllConfig {
         //  注意：每个用户信息的最后一项不需要加逗号！！！
         userList.add(getUser(
                 "oCH2_6muT7XkHHMKXjuVn5oO64uA", //扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "nYZXwthzRvppobNVlAC-a9tdZMHXZI3w-rJj1PAtTn0", //要给这个人发送的模板ID
+                "XvAkQexN0u1cAJkuK41AtBrMesjVikr-7TPQ0_mmU7Q", //要给这个人发送的模板ID
                 "毕浩然", //咋称呼这个人
                 "山东省菏泽市牡丹区", //这个人的详细地址
                 "菏泽", //这个人在的城市
@@ -81,31 +81,31 @@ public class AllConfig {
         ));
         
         
-        userList.add(getUser(
-                "oCH2_6iy8HNY0cp0xl46l8PQxuZI", //扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "nYZXwthzRvppobNVlAC-a9tdZMHXZI3w-rJj1PAtTn0", //要给这个人发送的模板ID
-                "丫丫", //咋称呼这个人
-                "山东济南市高新区", //这个人的详细地址
-                "济南", //这个人在的城市
-                new BirthDay(1999,2,15,true,false), //分别代表年、月、日、是否是农历(true为农历、false为公历)、是否统计天数(true为统计，false为倒计时)
-                new BirthDay(1999,8,11,false,false),
-                new BirthDay(2020,7,8,true,true),
-                new BirthDay(2020,7,8,true,false)
-        ));
+      //  userList.add(getUser(
+        //        "oCH2_6iy8HNY0cp0xl46l8PQxuZI", //扫码关注你的测试号以后，测试平台会出现TA的微信号
+         //       "nYZXwthzRvppobNVlAC-a9tdZMHXZI3w-rJj1PAtTn0", //要给这个人发送的模板ID
+         //       "丫丫", //咋称呼这个人
+        //        "山东济南市高新区", //这个人的详细地址
+       //         "济南", //这个人在的城市
+       //         new BirthDay(1999,2,15,true,false), //分别代表年、月、日、是否是农历(true为农历、false为公历)、是否统计天数(true为统计，false为倒计时)
+       //         new BirthDay(1999,8,11,false,false),
+       //         new BirthDay(2020,7,8,true,true),
+      //          new BirthDay(2020,7,8,true,false)
+      //  ));
         
         
         
-        userList.add(getUser(
-                "oCH2_6gOe_UF5KKU0D-8u069uXuE", //扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "4SOz-ArcU-JmTVhPBdYP1GpbKmFeuJND97Oeb64bZJk", //要给这个人发送的模板ID
-                "赵迎小朋友", //咋称呼这个人
-                "山东省青岛市即墨区", //这个人的详细地址
-                "青岛", //这个人在的城市
-                new BirthDay(1999,2,15,true,false), //分别代表年、月、日、是否是农历(true为农历、false为公历)、是否统计天数(true为统计，false为倒计时)
-                new BirthDay(1999,8,11,false,false),
-                new BirthDay(2020,7,8,true,true),
-                new BirthDay(2020,7,8,true,false)
-        ));
+       // userList.add(getUser(
+        //        "oCH2_6gOe_UF5KKU0D-8u069uXuE", //扫码关注你的测试号以后，测试平台会出现TA的微信号
+        //        "4SOz-ArcU-JmTVhPBdYP1GpbKmFeuJND97Oeb64bZJk", //要给这个人发送的模板ID
+        //        "赵迎小朋友", //咋称呼这个人
+       //         "山东省青岛市即墨区", //这个人的详细地址
+       //         "青岛", //这个人在的城市
+      //          new BirthDay(1999,2,15,true,false), //分别代表年、月、日、是否是农历(true为农历、false为公历)、是否统计天数(true为统计，false为倒计时)
+      //          new BirthDay(1999,8,11,false,false),
+      //          new BirthDay(2020,7,8,true,true),
+     //           new BirthDay(2020,7,8,true,false)
+      //  ));
         
 
 
