649a50ac52ced087ec5d2559d592a7d745b3a97e
bhr <93179048+544419203@users.noreply.github.com>
2022-11-05 14:06:11 +0800

    Update AllConfig.java

--- src/main/java/com/pt/vx/config/AllConfig.java
+++ src/main/java/com/pt/vx/config/AllConfig.java
@@ -70,7 +70,7 @@ public class AllConfig {
         //  注意：每个用户信息的最后一项不需要加逗号！！！
         userList.add(getUser(
                 "oCH2_6muT7XkHHMKXjuVn5oO64uA", //扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "x6S3kgdT-GNrX0lq0gssEeTTuT9yFPm6W5oL6keA0zA", //要给这个人发送的模板ID
+                "JughEv-qowvShAfJPY6GbTxX6y0R6uZYCzfPV3HI0DA", //要给这个人发送的模板ID
                 "毕浩然", //咋称呼这个人
                 "山东省菏泽市牡丹区", //这个人的详细地址
                 "菏泽", //这个人在的城市
